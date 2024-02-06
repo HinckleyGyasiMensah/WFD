c6bc282aefd27ade505d974a2a1e8b1c03b7a8a2
HinckleyGyasiMensah <159149150+HinckleyGyasiMensah@users.noreply.github.com>
2024-02-06 13:53:56 +0000

    Add files via upload

---
+++ lesson3/script/script.js
@@ -0,0 +1,27 @@
+/* do while loop
+let user;
+do {
+  user = window.prompt("Enter your name");
+} 
+  while(user == "");
+  console.log("Hello",user);*/
+//let user;
+  //user =window.prompt("Enter your name")
+  //while(user == "") {
+    //user = window.prompt("Enter your name");
+  //}
+  //console.log("Hello",user);
+
+ /* for (let counter = 1; counter <= 10; counter += 1)
+  {console.log("Happy New Year");
+} */
+
+let usernumber = ("Enter a number:");
+
+if (isNaN(usernumber)) (
+  console.log()
+)
+
+
+
+for (let i = 1; i <= 12 ; i++)
